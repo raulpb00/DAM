058b089e3e8e1df760d522f0f6b74c25fed29f2c
Raúl Prieto <raulpb00@gmail.com>
2017-10-04 12:09:05 +0200

    Update App_Demo1.cs

--- 04-09-2017/App_Demo1.cs
+++ 04-09-2017/App_Demo1.cs
@@ -4,19 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace App_Demo1
+namespace App_Demo1                      //Espacio de Nombre 
 {
-    class Program
+    class Program                      //Clase
     {
-        static void Main(string[] args)
+        static void Main(string[] args)                //Función MAIN
         {
-            Console.WriteLine("Hola soy el metodo que escribe");
+            Console.WriteLine("Hola soy el metodo que escribe");            //WriteLine salta de linea después de escribir.
             Console.WriteLine("==============================");
             Console.WriteLine();
-            Console.Write("El valor de PI es: ");
+            Console.Write("El valor de PI es: ");                           //Write sólo, deja el cursor después del texto.
             Console.WriteLine(3.141592);
 
-            Console.ReadLine();
+            Console.ReadLine();        //Se queda esperando una entrada por teclado, recibe lo que le metas, pero no hace nada con ello.
         }
     }
 }
